c346d71c5aa4c753daa0fc984bedb3a9404f680d
Reece Devenney <reecedevenney@gmail.com>
2023-03-04 15:15:37 -0500

    created getemployee and printemployee methods

--- Program.cs
+++ Program.cs
@@ -4,26 +4,39 @@ using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
+    {
+    static List<string> GetEmployees()
     {
         List<string> employees = new List<string>();
         while (true)
         {
-            Console.WriteLine("Please enter a name: ");
-            string input = Console.ReadLine() ?? "";
-            if (input == "")
-            {
-                break;
-            }
-            employees.Add(input);
-        }
-        
-        for(int i = 0; i < employees.Count; i++)
+
+        Console.WriteLine("Please enter a name: (leave empty to exit): ");
+
+        string input = Console.ReadLine() ?? "";
+
+        if (input == "")
         {
-            Console.WriteLine(employees[i]);
+            break;
+        }
+        employees.Add(input);
+        }
+        return employees;
+    }
+
+    static void PrintEmployees(List<string> employees)
+    {
+        for (int i = 0; i < employees.Count; i++)
+        {
+        Console.WriteLine(employees[i]);
         }
     }
-  }
+
+    static void Main(string[] args)
+    {
+        List<string> employees = GetEmployees();
+        PrintEmployees(employees);
+    }
+    }
 }
