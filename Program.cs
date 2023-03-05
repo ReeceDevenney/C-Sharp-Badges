175eba5a3491c6a98b85f35e6893cb77acf4fe7b
Reece Devenney <reecedevenney@gmail.com>
2023-03-05 14:48:59 -0500

    added util method to save data into csv file

--- Program.cs
+++ Program.cs
@@ -5,43 +5,35 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
-    static List<Employee> GetEmployees()
-    {
-        List<Employee> employees = new List<Employee>();
-        while (true)
+        static List<Employee> GetEmployees()
         {
-        Console.WriteLine("Please enter a name: (leave empty to exit): ");
-        string firstName = Console.ReadLine() ?? "";
-        if (firstName == "")
+            List<Employee> employees = new List<Employee>();
+            while (true)
+            {
+                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+                string firstName = Console.ReadLine() ?? "";
+                if (firstName == "")
+                {
+                    break;
+                }
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine() ?? "";
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine() ?? "");
+                Console.Write("Enter Photo URL:");
+                string photoUrl = Console.ReadLine() ?? "";
+
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
+            }
+            return employees;
+        }
+
+        static void Main(string[] args)
         {
-            break;
+            List<Employee> employees = GetEmployees();
+            Util.PrintEmployees(employees);
+            Util.MakeCSV(employees);
         }
-        Console.Write("Enter last name: ");
-        string lastName = Console.ReadLine() ?? "";
-        Console.Write("Enter ID: ");
-        int id = Int32.Parse(Console.ReadLine() ?? "");
-        Console.Write("Enter Photo URL:");
-        string photoUrl = Console.ReadLine() ?? "";
-
-        Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-        employees.Add(currentEmployee);
-        }
-        return employees;
-    }
-
-    static void PrintEmployees(List<Employee> employees)
-    {
-        for (int i = 0; i < employees.Count; i++)
-        {
-        string template = "{0,-10}\t{1,-20}\t{2}";
-        Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
-        }
-    }
-
-    static void Main(string[] args)
-    {
-        List<Employee> employees = GetEmployees();
-        PrintEmployees(employees);
-    }
     }
 }
